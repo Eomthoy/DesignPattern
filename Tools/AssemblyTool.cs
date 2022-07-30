78d63944bc716f18507ec4300bedab801e53bfac
程恩蒙 <2512410481@qq.com>
2022-07-30 18:41:40 +0800

    工厂模式

---
+++ Tools/AssemblyTool.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Tools
+{
+    /// <summary>
+    /// 反射工具类
+    /// </summary>
+    public class ReflectionTool
+    {
+        /// <summary>
+        /// 获取实现接口的所有实例
+        /// </summary>
+        /// <typeparam name="T">接口类型</typeparam>
+        /// <returns></returns>
+        public static List<T> GetInterfaceImplList<T>()
+        {
+            var result = new List<T>();
+
+            var a = typeof(T);
+
+            foreach (var type in Assembly.GetAssembly(typeof(T)).GetTypes())
+            {
+                if (type.IsInterface == false && type.GetInterfaces().Contains(typeof(T)))
+                {
+                    var impl = (T)Activator.CreateInstance(type);
+                    result.Add(impl);
+                }
+            }
+
+            return result;
+        }
+    }
+}
